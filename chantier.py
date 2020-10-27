8c3e1c2f3c3f58892929c9f515f85a9bb81e6244
vidanm <vidan.tauriff@gmail.com>
2020-10-27 12:40:05 +0100

    j'avais oublie le git

--- chantier.py
+++ chantier.py
@@ -2,9 +2,31 @@ import pandas as pd
 import datetime as dt
 from poste import *
 
-chantier = pd.read_excel("~/Documents/DGB/Resultat_chantier/chantier/19-GP-ROSN.xlsx")
-chantier = chantier.drop(columns=['Type','Référence interne','Date réf. externe','Auxiliaire','N°','Section analytique'])
+def AssocieChantierComptable(dicComptable,dfChantier):
+    d = { 
+            'Sous Poste' : [],
+            'Budget' : [],
+            'Dépenses du mois' : [],
+            'Dépenses cumulées' : [],
+            'RAD' : [],
+            'PFDC' : [],
+            'PFDC/Budget':[]
+        }
 
-dicAccountSite,dicAccountStruct = RecupDictComptable("/home/vidan/Documents/DGB/Resultat_chantier/plan comptable/PLAN COMPTABLE DGB 2020.xlsx")
+    postes = {'MO':d}
 
-print(chantier)
+    for index,value in dfChantier['Général'].iteritems():
+        row = dicComptable[str(value)].T.squeeze()
+        print(str(row)+"\n")
+        
+    return;
+
+dicComptable,dicComptStruct = RecupDictComptable("/home/vidan/Documents/DGB/Resultat_chantier/plan comptable/PLAN COMPTABLE DGB 2020.xlsx")
+
+dfChantier = RecupDfChantier("/home/vidan/Documents/DGB/Resultat_chantier/chantier/19-GP-ROSN.xlsx")
+
+#print(dfChantier)
+#print(dfAccountSite)
+#print(dfAccountSite[dfAccountSite['N° DE COMPTE'] == 604922]['SOUS POSTE'])
+AssocieChantierComptable(dicComptable,dfChantier)
+
--- poste.py
+++ poste.py
@@ -1,11 +1,9 @@
 import os
 import pandas as pd
 
+
 def RecupDictComptable(path):
     
-    """ renvoie un tuple contenant les 2 dictionnaires de poste comptables
-    Celui de la structure et celui des chantiers """
-
     dfAccountSite = pd.read_excel(path,header=1,usecols="A:D")
     dfAccountSite[['POSTE']] = dfAccountSite[['POSTE']].fillna(method='ffill')
 
@@ -13,28 +11,46 @@ def RecupDictComptable(path):
     dfAccountStruct[['POSTE.1']] = dfAccountStruct[['POSTE.1']].fillna(method='ffill')
 
     #On supprime toutes les lignes avec un sous poste nul
-    dfAccountSite = dfAccountSite[pd.notnull(dfAccountSite['SOUS POSTE'])]
-    dfAccountStruct = dfAccountStruct[pd.notnull(dfAccountStruct['SOUS POSTE.1'])]
+    dfAccountSite = dfAccountSite[pd.notnull(dfAccountSite['N° DE COMPTE'])]
+    dfAccountStruct = dfAccountStruct[pd.notnull(dfAccountStruct['N° DE COMPTE.1'])]
 
-    #On itere sur le plan, on instancie une dataframe pour chaque poste différent
+    """
+    
+    renvoie un tuple contenant les 2 dictionnaires de poste comptables
+    Celui de la structure et celui des chantiers
+
+    """
+
+    #On itere sur le plan, on instancie une dataframe pour chaque numéro de compte
+    #différent,
     #Que l'on stocke dans un dictionnaire
     dicPosteSite = {}
     dicPosteStruct = {}
-    prevPoste = ""
     for index, row in dfAccountSite.iterrows():
-        if prevPoste != row['POSTE']:
-            dfTemp = dfAccountSite.loc[dfAccountSite['POSTE'] == row['POSTE']]
-            dfTemp = dfTemp.drop(columns='POSTE')
-            #On enleve la colonne poste pour éviter la répétition
-            dicPosteSite[row['POSTE']] = dfTemp
-            prevPoste = row['POSTE']
+        dfTemp = dfAccountSite.loc[dfAccountSite['N° DE COMPTE'] == row['N° DE COMPTE']]
+        dfTemp = dfTemp.drop(columns='N° DE COMPTE')
+
+        if ('/' in str(row['N° DE COMPTE'])):
+            for num in str(row['N° DE COMPTE']).replace(" ","").split("/"):
+                dicPosteSite[num] = dfTemp
+        else: 
+            dicPosteSite[str(row['N° DE COMPTE'])] = dfTemp
 
     for index, row in dfAccountStruct.iterrows():
-        if prevPoste != row['POSTE.1']:
-            dfTemp = dfAccountStruct.loc[dfAccountStruct['POSTE.1'] == row['POSTE.1']]
-            dfTemp = dfTemp.drop(columns='POSTE.1')
-            dicPosteStruct[row['POSTE.1']] = dfTemp
-            prevPoste = row['POSTE.1']
+        dfTemp = dfAccountStruct.loc[dfAccountStruct['N° DE COMPTE.1'] == row['N° DE COMPTE.1']]
+        dfTemp = dfTemp.drop(columns='N° DE COMPTE.1')
+               
+        if ('/' in str(row['N° DE COMPTE.1'])):
+            for num in str(row['N° DE COMPTE.1']).replace(" ","").split("/"):
+                dicPosteSite[num] = dfTemp
+        else: 
+            dicPosteSite[row['N° DE COMPTE.1']] = dfTemp
 
     return (dicPosteSite,dicPosteStruct)
+
+
+def RecupDfChantier(path):
+    chantier = pd.read_excel(path)
+    chantier = chantier.drop(columns=['Type','Référence interne','Date réf. externe','Auxiliaire','N°','Section analytique'])
+    return chantier
 
