dd27ce89b2cc4913ca6239da622a45e5a3879372
vidanm <vidan.tauriff@gmail.com>
2020-10-28 13:55:27 +0100

    Association fichier chantier plan comptable

--- chantier.py
+++ chantier.py
@@ -3,30 +3,32 @@ import datetime as dt
 from poste import *
 
 def AssocieChantierComptable(dicComptable,dfChantier):
-    d = { 
-            'Sous Poste' : [],
-            'Budget' : [],
-            'Dépenses du mois' : [],
-            'Dépenses cumulées' : [],
-            'RAD' : [],
-            'PFDC' : [],
-            'PFDC/Budget':[]
+    data = { 
+            'Budget' : "",
+            'Dépenses du mois' : 0,
+            'Dépenses cumulées' : 0,
+            'RAD' : 0,
+            'PFDC' : 0,
+            'PFDC/Budget':0
         }
-
-    postes = {'MO':d}
+    
+    sousPostes = {}
+    postes = {}
 
     for index,value in dfChantier['Général'].iteritems():
         row = dicComptable[str(value)].T.squeeze()
-        print(str(row)+"\n")
-        
-    return;
+        poste = row[0]
+        sousPoste = row[1]
+
+        if (poste not in postes):
+            postes[poste] = sousPostes.copy()
+            if (sousPoste not in postes[poste]):
+                postes[poste][sousPoste] = data.copy()
+
+    return postes;
+
 
 dicComptable,dicComptStruct = RecupDictComptable("/home/vidan/Documents/DGB/Resultat_chantier/plan comptable/PLAN COMPTABLE DGB 2020.xlsx")
-
 dfChantier = RecupDfChantier("/home/vidan/Documents/DGB/Resultat_chantier/chantier/19-GP-ROSN.xlsx")
 
-#print(dfChantier)
-#print(dfAccountSite)
-#print(dfAccountSite[dfAccountSite['N° DE COMPTE'] == 604922]['SOUS POSTE'])
 AssocieChantierComptable(dicComptable,dfChantier)
-
