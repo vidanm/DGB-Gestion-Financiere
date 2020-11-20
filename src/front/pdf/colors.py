5b07ea435c81bd3d478cd4e6ec281dc75c07daa7
vidanm <vidan.tauriff@gmail.com>
2020-11-20 09:56:12 +0100

    Generation de tout les pdf de postes

---
+++ src/front/pdf/colors.py
@@ -0,0 +1,6 @@
+from reportlab.lib import colors
+
+dark = colors.Color(48/255,48/255,48/255)
+lightgrey = colors.Color(220/255,220/255,220/255)
+lightwhite = colors.Color(245/255,245/255,245/255)
+
