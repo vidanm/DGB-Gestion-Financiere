673859133d8f6c2db82dc1d604a60603034c6a80
vidan <vidan@manjaro.localdomain>
2021-02-05 12:25:53 +0100

    init

--- app/__init__.py
+++ app/__init__.py
@@ -46,8 +46,10 @@ def check_file_here():
     for filename in os.listdir('var'):
         print(filename)
         if 'plan.xls' == filename:
+            print("planoui")
             plan = PlanComptable('var/plan.xls')
         if 'charges.xls' == filename and plan != None:
+            print("chargesoui")
             charges = Charges('var/charges.xls',plan,codes_missing)
         if 'budget.xls' == filename:
             budget = read_budget('var/budget.xls')
