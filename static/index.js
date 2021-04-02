6149825eb18793e0acd2c8423529cef8544f782e
vidanm <vidanmurati@hotmail.fr>
2021-04-02 11:12:43 +0200

    redirects

--- static/index.js
+++ static/index.js
@@ -7,21 +7,6 @@ function showChantier(id) {
 	}
 }
 
-function requiredFiles(){
-	var fs = require('fs');
-	var numberOfRequiredFiles = 0
-	var files = fs.readdirSync('var/')
-	files.forEach(function(element) {
-		if (element == "PlanComptable.xls" || element == "Charges.xls") {
-			numberOfRequiredFiles += 1;
-		}
-	});
-
-	if (numberOfRequiredFiles == 2)
-		return true;
-	return false;
-}
-
 window.addEventListener("load", function(){
 	var x = document.getElementById("syntheseform");
 	x.style.display = "none";
@@ -35,9 +20,7 @@ window.addEventListener("load", function(){
 	var buttonChantier = document.getElementById("buttonChantier");
 	var buttonSynthese = document.getElementById("buttonSynthese");
 	var buttonStructure = document.getElementById("buttonStructure");
-	if ( requiredFiles() == false ){
-		buttonChantier.disabled = true;
-		buttonSynthese.disabled = true;
-		buttonStructure.disabled = true;
-	}
+	/* buttonChantier.disabled = true;
+	buttonSynthese.disabled = true;
+	buttonStructure.disabled = true; */
 });
